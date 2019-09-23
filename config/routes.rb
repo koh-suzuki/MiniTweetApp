fac94d56d890b3ef2e207543dd1855f263956aad
koh-suzuki <k.s_coward_0629@icloud.com>
2019-09-23 12:42:05 +0000

    3章Finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'home#top'
+  get 'top', to: 'home#top'
   get 'about', to: 'home#about'
+  get 'posts/new', to: 'posts#new'
+  post 'posts/create', to: 'posts#create'
+  get 'posts/index', to: 'posts#index'
+  get 'posts/:id', to: 'posts#show', as: :post
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
